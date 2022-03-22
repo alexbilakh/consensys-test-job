180e47a04b276b14c7846a2407356cbc8906e054
alexbilakh <oskar.kamil121@gmail.com>
2022-03-22 20:03:31 +0900

    Fixed getRoomUsers issue

--- contracts/RoomBooking.sol
+++ contracts/RoomBooking.sol
@@ -15,24 +15,39 @@ contract RoomBooking {
         Company company;
     }
 
-    address[] public users;
+    struct UserData {
+        address userAddress;
+        uint32 bookedAt;
+    }
+
+    UserData[] public users;
 
     // saves room number of user (real room number = value + 1)
     mapping(address => uint8) private userRoom;
 
     // rooms data with user-count and company type
-    RoomData[ROOM_COUNT] public rooms;
+    RoomData[ROOM_COUNT] private rooms;
 
     event BookedRoom(uint8 room, address booker, Company company);
     event CanceledReservation(uint8 room, address booker, Company company);
 
     modifier onlyBookedUser(address _addr) {
-        require(userRoom[_addr] >= uint8(0x0), "Not booked yet");
+        if (
+            userRoom[_addr] < uint8(0x0)
+            || userRoom[_addr] >= ROOM_COUNT
+        ) {
+            revert("Not booked yet");
+        }
         _;
     }
 
     modifier onlyLimitedRooms(uint8 _roomNumber) {
-        require(_roomNumber < ROOM_COUNT, "Room is not exist");
+        if (
+            _roomNumber >= ROOM_COUNT
+            || _roomNumber < 0
+        ) {
+            revert("Room is not exist");
+        }
         _;
     }
 
@@ -51,21 +66,21 @@ contract RoomBooking {
         public
         view
         onlyLimitedRooms(_roomNumber)
-        returns (address[] memory)
+        returns (UserData[] memory roomUsers, uint8 userCount, Company company)
     {
-        address[] memory roomUsers = new address[](
+        UserData[] memory _roomUsers = new UserData[](
             rooms[_roomNumber].userCount
         );
 
-        uint8 roomUserCount = 0;
+        uint8 _roomUserCount = 0;
         for (uint256 i = 0; i < users.length; i++) {
-            if ((userRoom[users[i]] - 1) == _roomNumber) {
-                roomUsers[roomUserCount] = users[i];
-                roomUserCount++;
+            if ((userRoom[users[i].userAddress] - 1) == _roomNumber) {
+                _roomUsers[_roomUserCount] = users[i];
+                _roomUserCount++;
             }
         }
 
-        return roomUsers;
+        return (_roomUsers, rooms[_roomNumber].userCount, rooms[_roomNumber].company);
     }
 
     // book a room by room number and comapny
@@ -79,12 +94,20 @@ contract RoomBooking {
         ) {
             revert("Another company owned the room");
         }
-        require(userRoom[msg.sender] == uint8(0x0), "Already booked a room");
+        if (
+            userRoom[msg.sender] != uint8(0x0)
+            && userRoom[msg.sender] != 100
+        ) {
+            revert("Already booked a room");
+        }
 
         rooms[_roomNumber].userCount++;
         rooms[_roomNumber].company = company;
         userRoom[msg.sender] = _roomNumber + 1;
-        users.push(msg.sender);
+        users.push(UserData({
+            userAddress: msg.sender,
+            bookedAt: uint32(block.timestamp)
+        }));
 
         emit BookedRoom(_roomNumber, msg.sender, company);
     }
@@ -93,12 +116,21 @@ contract RoomBooking {
     function cancelReservation() public onlyBookedUser(msg.sender) {
         RoomData storage targetRoom = rooms[userRoom[msg.sender] - 1];
         targetRoom.userCount--;
+        
+        // mark as deleted in userRoom
+        userRoom[msg.sender] = 100;
+
+        // remove from users list
+        uint8 _userIndex;
+        for (uint8 i = 0; i < users.length; i++) {
+            if (users[i].userAddress == msg.sender) {
+                _userIndex = i;
+                break;
+            }
+        }
+        users[_userIndex] = users[users.length - 1];
+        users.pop();
 
         emit CanceledReservation(userRoom[msg.sender] - 1, msg.sender, targetRoom.company);
-
-        if (targetRoom.userCount == 0) {
-            delete targetRoom.company;
-        }
-        delete userRoom[msg.sender];
     }
 }
